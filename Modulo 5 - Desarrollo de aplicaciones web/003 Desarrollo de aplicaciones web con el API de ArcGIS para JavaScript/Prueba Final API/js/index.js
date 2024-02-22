5bded312265c097c05a90e0ac9c0df6b5d8ce451
Ines Macias <ines@phydelta.net>
2024-02-22 07:37:43 +0000

    Se añaden nuevas funcionalidades

--- Modulo 5 - Desarrollo de aplicaciones web/003 Desarrollo de aplicaciones web con el API de ArcGIS para JavaScript/Prueba Final API/js/index.js
+++ Modulo 5 - Desarrollo de aplicaciones web/003 Desarrollo de aplicaciones web con el API de ArcGIS para JavaScript/Prueba Final API/js/index.js
@@ -1,14 +1,37 @@
 
-const mapContainer = document.getElementById("map-container");
+/**
+ * Archivo principal de la aplicación.
+ * Este archivo contiene la lógica principal para crear y controlar el mapa, así como agregar capas, widgets y funcionalidades adicionales.
+ */
 
-const mapController = new MapController(mapContainer);
-mapController.AddDynamicLayer("https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer");
+const mapContainer = document.getElementById("map-container");
+const mapController = new MapController(mapContainer, "https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer");
+
+//mapController.AddDynamicLayer("https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer");
 
 mapController.AddLeyenda(document.getElementById("leyenda-container"));
 mapController.AddSearchWidget();
 mapController.AddBaseMapoGallery();
 mapController.AddScaleBar();
-mapController.AddOverviewMap();
 
 const overviewMapContainer = document.getElementById("overviewDiv");
 mapController.AddOverviewMap(overviewMapContainer);
+
+mapController.InitSelectCityFeatures();
+
+/**
+ * Evento de clic para el botón "btoGotoEstado".
+ * Obtiene el valor seleccionado del elemento "cboEstado" y utiliza el controlador del mapa para navegar al estado correspondiente.
+ */
+const btoGotoEstado = document.getElementById("btoGotoEstado");
+btoGotoEstado.addEventListener("click", () => {
+    const cboEstado = document.getElementById("cboEstado");
+    const selectedValue = cboEstado.value;
+    mapController.GotoEstado(selectedValue);
+});
+
+const btoSelectCiudades = document.getElementById("btoSelectCiudades");
+btoSelectCiudades.addEventListener("click", () => {
+    mapController.StartCitiesSelection();
+});
+
--- Modulo 5 - Desarrollo de aplicaciones web/003 Desarrollo de aplicaciones web con el API de ArcGIS para JavaScript/Prueba Final API/js/map_controller.js
+++ Modulo 5 - Desarrollo de aplicaciones web/003 Desarrollo de aplicaciones web con el API de ArcGIS para JavaScript/Prueba Final API/js/map_controller.js
@@ -1,19 +1,48 @@
-class MapController {
-    
 
-    constructor(containerElement) {
+/**
+ * Clase que representa el controlador del mapa.
+ */
+class MapController {
+
+    /**
+     * Constructor de la clase MapController.
+     * @param {HTMLElement} containerElement - Elemento contenedor del mapa.
+     */
+    constructor(containerElement, url) {
         require([
             "esri/Map",
             "esri/views/MapView",
+            "esri/layers/MapImageLayer",
             "esri/config"
-        ], (Map, MapView, config) => {
+        ], (Map, MapView, MapImageLayer, config) => {
 
             config.apiKey = "AAPK67942d4773a94924a7c03d707148d230cvm3Td6tC1gUSyijET8f2Y2YHgaQnpXVGVS_pQWOfwWzidth8Dp6OYK4Czr1jQSN";
             config.portalUrl = "https://www.arcgis.com";
 
+
+            this.dynamicLayer = new MapImageLayer({
+                url: url
+            });
+
+            //Se utiliza el método when para esperar a que la capa dinámica esté lista para ser utilizada.
+            this.dynamicLayer.when().then(() => {
+                this.dynamicLayer.sublayers.forEach((sublayer) => {
+                    if (sublayer.id === 0) {
+                        sublayer.popupTemplate = this.ciudadTemplate;
+                    };
+                    if (sublayer.id === 2) {
+                        sublayer.popupTemplate = this.estadoTemplate;
+                    };
+                });
+                this.view.whenLayerView(this.dynamicLayer).then((layerView) => {
+                    this.dynamicLayerView = layerView;
+                });
+            });            
+
             this.mapMain = new Map({
                 basemap: "topo",
-                ground: "world-elevation"
+                ground: "world-elevation",
+                layers: [this.dynamicLayer]
             });
 
             this.view = new MapView({
@@ -21,11 +50,99 @@ class MapController {
                 map: this.mapMain,
                 center: [-97.5, 38.5],
                 zoom: 4
-            });
+            });    
 
+            
         });
     }
 
+    /**
+     * Plantilla de información para el estado.
+     * @type {Object}
+     */
+    estadoTemplate = {
+        title: "Información del estado",
+        content: [{
+          type: "fields",
+          fieldInfos: [{
+            fieldName: "STATE_NAME",
+            label: "Nombre del estado",
+            visible: true
+          }, {
+            fieldName: "POP2000",
+            label: "Población 2000",
+            visible: true
+          }, {
+            fieldName: "POP00_SQMI",
+            label: "Población por milla cuadrada en 2000",
+            visible: true
+          }, {
+            fieldName: "AREA",
+            label: "Área",
+            visible: true
+          }]
+        }]
+      };   
+
+    /**
+     * Plantilla de información para la ciudad.
+     * @type {Object}
+     */
+    ciudadTemplate = {
+    title: "Información de la ciudad",
+    content: [{
+        type: "fields",
+        fieldInfos: [{
+        fieldName: "AREANAME",
+        label: "Nombre",
+        visible: true
+        },{
+        fieldName: "ST",
+        label: "Estado",
+        visible: true
+        },{
+        fieldName: "POP2000",
+        label: "Población 2000",
+        visible: true
+        }]
+    }]
+    };   
+      
+    // /**
+    //  * Agrega una capa dinámica al mapa.
+    //  * @param {string} url - URL de la capa dinámica.
+    //  */
+    // AddDynamicLayer(url) {
+    //     require([
+    //         "esri/layers/MapImageLayer"
+    //     ], (MapImageLayer) => {
+    //         this.dynamicLayer = new MapImageLayer({
+    //             url: url
+    //         });
+
+    //         //Se utiliza el método when para esperar a que la capa dinámica esté lista para ser utilizada.
+    //         this.dynamicLayer.when().then(() => {
+    //             this.dynamicLayer.sublayers.forEach((sublayer) => {
+    //                 if (sublayer.id === 0) {
+    //                     sublayer.popupTemplate = this.ciudadTemplate;
+    //                 };
+    //                 if (sublayer.id === 2) {
+    //                     sublayer.popupTemplate = this.estadoTemplate;
+    //                 };
+    //             });
+    //             this.view.whenLayerView(this.dynamicLayer).then((layerView) => {
+    //                 this.dynamicLayerView = layerView;
+    //             });
+    //         });
+
+    //         this.mapMain.add(this.dynamicLayer);
+    //     });
+    // }
+
+    /**
+     * Agrega una leyenda al mapa.
+     * @param {HTMLElement} leyendaContainer - Elemento contenedor de la leyenda.
+     */
     AddLeyenda(leyendaContainer) {
         require([
             "esri/widgets/Legend"
@@ -39,18 +156,10 @@ class MapController {
         });
     }
 
-    AddDynamicLayer(url) {
-        require([
-            "esri/layers/MapImageLayer"
-        ], (MapImageLayer) => {
-            this.dynamicLayer = new MapImageLayer({
-                url: url
-            });
-
-            this.mapMain.add(this.dynamicLayer);
-        });
-    }
-
+    /**
+     * Agrega un widget de búsqueda al mapa.
+     * @param {HTMLElement} searchContainer - Elemento contenedor del widget de búsqueda.
+     */
     AddSearchWidget(searchContainer) {
         require([
             "esri/widgets/Search"
@@ -66,6 +175,10 @@ class MapController {
         });
     }
 
+    /**
+     * Agrega una galería de mapas base al mapa.
+     * @param {HTMLElement} basemapGalleryContainer - Elemento contenedor de la galería de mapas base.
+     */
     AddBaseMapoGallery(basemapGalleryContainer) {
         require([
             "esri/widgets/BasemapGallery",
@@ -82,14 +195,19 @@ class MapController {
                 var expand = new Expand({
                     expandIconClass: "esri-icon-basemap",
                     view: this.view,
-                    content: basemapGallery
+                    content: basemapGallery,
+                    expandTooltip: "Basemap"
                 });
     
-                this.view.ui.add( expand, "top-left");
+                this.view.ui.add( expand, "top-right");
             }
         });
     }
 
+    /**
+     * Agrega una barra de escala al mapa.
+     * @param {HTMLElement} scaleBarContainer - Elemento contenedor de la barra de escala.
+     */
     AddScaleBar(scaleBarContainer) {
         require([
             "esri/widgets/ScaleBar"
@@ -100,11 +218,15 @@ class MapController {
             });
 
             if (!scaleBarContainer) {
-                this.view.ui.add(this.scaleBar, "bottom-left");
+                this.view.ui.add(this.scaleBar, "bottom-right");
             }
         });
     }
 
+    /**
+     * Agrega un mapa de vista general al mapa.
+     * @param {HTMLElement} overviewMapContainer - Elemento contenedor del mapa de vista general.
+     */
     AddOverviewMap(overviewMapContainer) {
         require([
             "esri/Map",
@@ -122,7 +244,7 @@ class MapController {
                 container: overviewMapContainer,
                 map: overviewMap,
                 center: [-97.5, 38.5],
-                zoom: 1,                
+                zoom: 2,                
                 constraints: {
                     rotationEnabled: false
                 }
@@ -156,22 +278,141 @@ class MapController {
             });
 
             const extentDebouncer = promiseUtils.debounce(async () => {
-                if (this.view.stationary) {
-                  await overviewMapView.goTo({
-                    center: this.view.center,
-                    scale:
-                    this.view.scale *
-                      2 *
-                      Math.max(
-                        this.view.width / overviewMapView.width,
-                        this.view.height / overviewMapView.height
-                      )
-                  });
+                if (this.view.stationary){
+                    await overviewMapView.goTo({
+                        center: this.view.center,
+                        scale: this.view.scale * 2 * Math.max(
+                                this.view.width / overviewMapView.width,
+                                this.view.height / overviewMapView.height
+                            )
+                      });
                 }
               });            
         });
     }
 
+    /**
+     * Realiza un zoom al estado especificado.
+     * @param {string} nombreEstado - Nombre del estado al que se desea hacer zoom.
+     */
+    GotoEstado(nombreEstado) {
+        require([
+            "esri/rest/query"         
+        ], (query) => {
+            query.where = `STATE_NAME = '${nombreEstado}'`;
+            query.outSpatialReference = this.mapMain.spatialReference;
+            query.returnGeometry = true;
+
+            const stateLayer = this.mapMain.layers.items[0].findSublayerById(2);
+            stateLayer.queryFeatures(query).then((result) => {
+                if (result.features.length > 0) {
+                    const stateExtent = result.features[0].geometry.extent;
+                    this.view.goTo(stateExtent);
+
+                    this.view.whenLayerView(this.dynamicLayer).then((layerView) => {
+                        if (this.estadoHighlight) {
+                            this.estadoHighlight.remove();
+                        } 
+                        layerView.highlightOptions = {
+                            color: [236, 233, 114, 1],
+                            haloColor : [255, 0, 0, 1],
+                            haloOpacity: 1,
+                            fillOpacity: 0.5
+                        };                        
+                        this.estadoHighlight = layerView.highlight(result.features[0]);
+                    });                    
+                } else {
+                    console.log("No se ha podido encontrar el estado");
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+        });        
+    }
+
+
+    InitSelectCityFeatures() {
+        require([
+            "esri/layers/GraphicsLayer",            
+            "esri/widgets/Sketch/SketchViewModel",
+            "esri/geometry/geometryEngineAsync"            
+        ], (GraphicsLayer,SketchViewModel, geometryEngineAsync) => {
+
+            this.polygonGraphicsLayer = new GraphicsLayer();
+            this.mapMain.add(this.polygonGraphicsLayer);
+
+            this.sketchViewModel = new SketchViewModel({
+                view: this.view,
+                layer: this.polygonGraphicsLayer
+            });
+
+
+            // Once user is done drawing a rectangle on the map
+            // use the rectangle to select features on the map and table
+            this.sketchViewModel.on("create", async (event) => {
+                if (event.state === "complete") {
+                    // this polygon will be used to query features that intersect it
+                    const geometries = this.polygonGraphicsLayer.graphics.map(function (graphic) {
+                    return graphic.geometry;
+                });
+                const queryGeometry = await geometryEngineAsync.union(geometries.toArray());
+                //const citySubLayer = this.mapMain.layers.items[0].findSublayerById(0);
+                selectFeatures(this.view, this.dynamicLayer, queryGeometry, this.citiesHighlight);
+                }
+            });
+
+
+            // This function is called when user completes drawing a rectangle
+            // on the map. Use the rectangle to select features in the layer and table
+            function selectFeatures(view, dynamicLayer, geometry, citiesHighlight) {
+                const citySubLayer = dynamicLayer.findSublayerById(0);                
+                if (citySubLayer) {
+                    // create a query and set its geometry parameter to the
+                    // rectangle that was drawn on the view
+                    const query = {
+                        geometry: geometry,
+                        outFields: ["*"]
+                    };
+
+                    // query graphics from the csv layer view. Geometry set for the query
+                    // can be polygon for point features and only intersecting geometries are returned
+                    citySubLayer
+                        .queryFeatures(query)
+                        .then((results) => {
+                            if (results.features.length === 0) {
+                                clearSelection();
+                            } else {
+                                view.whenLayerView(dynamicLayer).then((layerView) => {
+                                    if (citiesHighlight) {
+                                        citiesHighlight.remove();
+                                    } 
+                                    layerView.highlightOptions = {
+                                        color: [236, 233, 114, 1],
+                                        haloColor : [255, 0, 0, 1],
+                                        haloOpacity: 1,
+                                        fillOpacity: 0.5
+                                    };                        
+                                    citiesHighlight = layerView.highlight(results.features);
+                                    // layerView.featureEffect = {
+                                    //     filter: {
+                                    //         objectIds: results.features
+                                    //     },
+                                    //     excludedEffect: "blur(5px) grayscale(90%) opacity(40%)"
+                                    // };
+                                });
+                            }
+                        });
+                }
+            }            
+        });
+    }
+
+
+    StartCitiesSelection() {
+        // cierra el popup si está abierto
+        this.view.closePopup();        
+        this.sketchViewModel.create("polygon");
+    }
 
 }
 
